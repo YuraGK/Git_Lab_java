cdc7ebb13dd0016a0656c89ae7ee166e2bb79633
YuraGK <112947522+YuraGK@users.noreply.github.com>
2025-01-11 18:18:22 +0200

    Delete AtlassGym directory

--- AtlassGym/workload-service/src/main/java/com/WorkloadService/workload_service/repository/WorkloadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.WorkloadService.workload_service.repository;
-
-import com.WorkloadService.workload_service.model.TrainersMonthlyTrainings;
-import com.WorkloadService.workload_service.model.Workload;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkloadRepository extends MongoRepository<Workload, String> {
-}
